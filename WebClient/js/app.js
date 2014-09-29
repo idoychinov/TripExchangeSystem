d2d0b96563abf412f5d3ccebcda9f0dc06e13301
idoychinov <doychinov.ivan@gmail.com>
2014-09-30 01:27:22 +0300

    paths fixed

--- WebClient/js/app.js
+++ WebClient/js/app.js
@@ -13,33 +13,33 @@ var app = angular.module('myApp', ['ngRoute', 'ngResource', 'ngCookies']).
 
         $routeProvider
             .when('/', {
-                templateUrl: '/views/partials/home.html',
+                templateUrl: 'views/partials/home.html',
                 controller: 'HomeController'
             })
             .when('/register', {
-                templateUrl: '/views/partials/register.html',
+                templateUrl: 'views/partials/register.html',
                 controller: 'SignUpController'
             })
             .when('/trips', {
-                templateUrl: '/views/partials/trips.html',
+                templateUrl: 'views/partials/trips.html',
                 controller: 'TripsController'
             })
             .when('/drivers', {
-                templateUrl: '/views/partials/drivers.html',
+                templateUrl: 'views/partials/drivers.html',
                 controller: 'DriversController'
             })
             .when('/drivers/:id', {
-                templateUrl: '/views/partials/drivers-details.html',
+                templateUrl: 'views/partials/drivers-details.html',
                 controller: 'DriversDetailsController',
                 resolve: routeUserChecks.authenticated
             })
             .when('/trips/:id', {
-                templateUrl: '/views/partials/trips-details.html',
+                templateUrl: 'views/partials/trips-details.html',
                 controller: 'TripsDetailsController',
                 resolve: routeUserChecks.authenticated
             })
             .when('/unauthorized', {
-                templateUrl: '/views/partials/unauthorized.html'
+                templateUrl: 'views/partials/unauthorized.html'
             })
             .otherwise({ redirectTo: '/' });
     }])
